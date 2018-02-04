e1d09398c10e24ce5086216f404caaab1980ad5c
Davor Ocelic <docelic@spinlocksolutions.com>
2018-02-04 23:02:28 +0100

    Functional updates

--- support/run-diffs.bash
+++ support/run-diffs.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 ( echo "# If anything changes here, sync the items in shards.txt";
-rgrep -h ^require amber app |grep -v \\./|grep -v \\.js|sort|uniq ) > diffs/shards.txt
+rgrep -h ^require amber/src amber/lib/*/src app/src  |grep -v \\./|grep -v \\.js|sort|uniq ) > diffs/shards.txt
 
+git diff diffs
